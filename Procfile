1304d89eeb60ca854c7ac93e56876a996f871bf1
SaiPranavPasham <SaiPranavPasham@users.noreply.github.com>
2026-04-03 00:50:05 +0530

    Fix Procfile and start script

--- Procfile
+++ Procfile
@@ -1,9 +1,1 @@
 web: sh start.sh
-#!/bin/bash
-
-echo "Starting app..."
-
-export USERNAME=admin
-export PASSWORD=admin1234
-
-python manage.py runserver 0.0.0.0:$PORT
--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
-set -e
+#!/bin/bash
+
+echo "Starting app..."
 
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-gunicorn dashboard_project.wsgi:application --bind 0.0.0.0:${PORT:-8000}
+
+python manage.py runserver 0.0.0.0:$PORT
